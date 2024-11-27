2054c89aa6432be563e4edd61c9d2b08199ef8ed
Yehor Makarenko <mak.yehor.knu@gmail.com>
2024-11-27 08:36:03 +0200

    Fix

--- lab4/index.js
+++ lab4/index.js
@@ -1,14 +1,5 @@
 const express = require('express');
-const promClient = require('prom-client')
 const app = express();
-
-const collectDefaultMetrics = promClient.collectDefaultMetrics;
-collectDefaultMetrics();
-
-app.get('/metrics', async (req, res) => {
-    res.set('Content-Type', promClient.register.contentType);
-    res.end(await promClient.register.metrics());
-});
 
 app.get('/products/:productId', (req, res) => {
     const productId = req.params.productId;
